b8f9d3a85e42a8351084b9b7df60c07bf9d3a726
Ritu Chaudhary <rituchaudhary2424@gmail.com>
2026-04-12 10:18:18 -0400

    Time: 1439 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+with cte as 
+(select 
+s.user_id, 
+sum(case when (c.action='confirmed' or c.action is null) then 1 else 0 end ) as confirmcount,
+sum(case when c.action is not null then 1 else 0 end ) as totalcount
+from Signups s
+join Confirmations c
+on s.user_id=c.user_id
+group by 1)
+
+select user_id, round(confirmcount/totalcount,2) as confirmation_rate
+from cte
+UNION
+Select user_id, 0.00 as confirmation_rate
+from Signups where user_id not in (select distinct user_id from Confirmations)
